714a66e60e5fd7598968a564858f3dc5bb8d9771
Hendrik Laanemets <hlaaneme@tktk.ee>
2022-02-04 20:23:35 +0200

    Control Flow

---
+++ UserAge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm kuvab sünniaastat konsoolis;
+            //"oled sündinud aastal {yearOfBirth}";
+
+            Console.WriteLine("Palun sisesta oma vanus");
+
+            int UserAge = Convert.ToInt32(Console.ReadLine());
+
+            int YearOfBirth = 2021 - UserAge;
+
+
+            Console.WriteLine($"Oled sündinud aastal {YearOfBirth}");
+
+
+
+
+        }
+    }
+}
